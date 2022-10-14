b8309e192f8ac5ca1801c03cd2fcb2b3f90ac46a
Bori6 <boriapata@gmail.com>
2022-10-14 09:24:18 -1200

    first commit

--- cypress/integration/RegressionTest.js
+++ cypress/integration/RegressionTest.js
@@ -1,16 +1,35 @@
 /// <reference types= "cypress" />
 
-describe("Instagram Regression Test suite",  () => {
-  beforeEach(() => {
-    cy.OpenUrl({});
-  });
-
-  it("Validate that user with invalid details cannot login to the Instagram successfully",  () => {
+describe(" Quales course management Test suite",  () => {
   
-    //Login
-    cy.Login({});
-    //Assert login with wrong details.
-    cy.AssertLoginWrongDetails({});
+    
+
+
+  it("Validate that user can login into quales course management successfully",  () => {
+cy.visit("http://tawdry-rule.surge.sh")
+cy.viewport(1530,750)
+cy.get('.css-48p1y4 > .MuiButton-root').click()
+cy.get('[data-testid=EmailAddress]').type("ayp@mail.com")
+cy.get('[data-testid=Password]').type("pass1234")
+cy.get('.css-190uank').click()
+cy.get('.MuiButton-contained').click()
+cy.get('.MuiGrid-root > .MuiButton-root').click()
+cy.get('[data-testid="Title*"]').type("Bori")
+cy.get('[data-testid="Description*"]').type("best course 204")
+cy.get('#demo-simple-select').click()
+cy.get('[data-value="1"]').click()
+cy.get('[data-testid=isPremium]').click()
+cy.get('[data-testid=ImageURL]').type("https://en.wikipedia.org/wiki/Actor")
+cy.get('[data-testid=online]').click()
+cy.get('[data-testid="CourseURL*(mustbeyoutube)"]').type("https://www.youtube.com/watch?v=QhWDZe8t7YU")
+cy.get('.css-tzsjye > .MuiButton-root').click()
+cy.get('.css-tzsjye > .MuiButton-root').click()
+cy.get('.css-tzsjye > .MuiButton-root').click()
+cy.get('.MuiToolbar-root > .MuiButton-root').click()
+
+
+  
+  
   });
 
 });
