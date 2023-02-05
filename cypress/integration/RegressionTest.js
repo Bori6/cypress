78edfcc8c59128cdcc1646329e4aecefe2d2285b
Bori6 <boriapata@gmail.com>
2023-02-04 16:52:00 -1200

    TopSet Testscript

--- cypress/integration/RegressionTest.js
+++ cypress/integration/RegressionTest.js
@@ -1,35 +1,80 @@
-/// <reference types= "cypress" />
+/// <reference types='cypress' />
 
-describe(" Quales course management Test suite",  () => {
-  
-    
+import TopSet from "../pageObjects/topSetLoginPage.cy.js"
+const email="qa.analyst@email.com"
+const password="Password123!"
 
-
-  it("Validate that user can login into quales course management successfully",  () => {
-cy.visit("http://tawdry-rule.surge.sh")
-cy.viewport(1530,750)
-cy.get('.css-48p1y4 > .MuiButton-root').click()
-cy.get('[data-testid=EmailAddress]').type("ayp@mail.com")
-cy.get('[data-testid=Password]').type("pass1234")
-cy.get('.css-190uank').click()
-cy.get('.MuiButton-contained').click()
-cy.get('.MuiGrid-root > .MuiButton-root').click()
-cy.get('[data-testid="Title*"]').type("Bori")
-cy.get('[data-testid="Description*"]').type("best course 204")
-cy.get('#demo-simple-select').click()
-cy.get('[data-value="1"]').click()
-cy.get('[data-testid=isPremium]').click()
-cy.get('[data-testid=ImageURL]').type("https://en.wikipedia.org/wiki/Actor")
-cy.get('[data-testid=online]').click()
-cy.get('[data-testid="CourseURL*(mustbeyoutube)"]').type("https://www.youtube.com/watch?v=QhWDZe8t7YU")
-cy.get('.css-tzsjye > .MuiButton-root').click()
-
-
-
-
-
-  
-  
+describe(" web application test suite", () => {
+  beforeEach(function () {
+    //navigate to the Login page
+    cy.visit("https://topset-dev.vercel.app/login");
+    //assert that login page opened successfully
+    cy.url().should("include", "login");
   });
 
+  it("Validate that user can login with valid credentials into web application successfully", () => {
+    const Login = new TopSet();
+
+    //input Valid user email
+    Login.getEmail().clear().type(email);
+    //input valid user corresponding password
+    Login.getPassword().clear().type(password);
+    //click on Login button
+    Login.getSubmitButton().click();
+    //assert that Login was successful
+    cy.url().should("not.include", "login");
+  });
+
+  it("Validate that user cannot login with invalid email credentials", () => {
+    const Login = new TopSet();
+
+    //input invalid email
+    Login.getEmail().clear().type("qaaa.analyst@email.com");
+    //input valid user password
+    Login.getPassword().clear().type(password);
+    //click on Login button
+    Login.getSubmitButton().click();
+    //assert error message
+    Login.getWrongLogin().should("contain.text", "Invalid login details");
+  });
+
+  it("Validate that user cannot login with invalid password", () => {
+    const Login = new TopSet();
+
+    //input valid email
+    Login.getEmail().clear().type(email);
+    //input invalid user password
+    Login.getPassword().clear().type("Password1234!");
+    //click on Login button
+    Login.getSubmitButton().click();
+    //assert error message
+    Login.getWrongLogin().should("contain.text", "Invalid login details");
+  });
+
+  it("Validate error message is displayed when the required fields are left blank", () => {
+    const Login = new TopSet();
+
+    //click on Login button
+    Login.getSubmitButton().click();
+    //assert email error message
+    Login.getEmailBlankField().should("contain.text", "Email is required");
+    //assert password error message
+    Login.getPasswordError().should("contain.text", "Password is required");
+  });
+
+  it("Validate error message is displayed when password length does not meet minimum requirements", () => {
+    const Login = new TopSet();
+
+    //input Valid user email
+    Login.getEmail().clear().type(email);
+    //input password with length less than minimum requirements
+    Login.getPassword().clear().type("Pass");
+    //click on Login button
+    Login.getSubmitButton().click();
+    //assert password error message
+    Login.getPasswordError().should(
+      "contain.text",
+      "password must be at least 8 characters"
+    );
+  });
 });
